678840f2f099f5d808098a9e8e5775593dc7309e
filippo-mancinelli <filippo.mancinelli@studenti.unicam.it>
2022-12-10 12:16:23 +0100

    assets, styles, navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import './navbar.css';
+import logo from '../../assets/logo.svg';
+//BEM -> Block Element Modifier 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt3_navbar">
+      <div className="gpt3_navbar-links">
+        <div className='gpt3_navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className='gpt_3_navabar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is task-sama</a></p>
+          <p><a href='#possibility'>openAI</a></p>
+          <p><a href='#features'>demone</a></p>
+          <p><a href='#blog'>Library</a></p>
+        </div>
+      </div>
+
+    <div className='gpt3_navbar-sign'>
+      <p>Sign in</p>
+      <button type='button'>Sign up</button>
+    </div>
+
+    </div>
   )
 }
 
