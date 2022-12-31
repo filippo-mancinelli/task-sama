4eee9084d8fd2eaa935e504cd196e85b4dce2d08
filippo-mancinelli <filippo.mancinelli@studenti.unicam.it>
2022-12-31 19:57:04 +0100

    css searchbar

--- src/components/searchCollections/SearchCollections.jsx
+++ src/components/searchCollections/SearchCollections.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const SearchCollections = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function handleSubmit(event){
+    event.preventDefault();
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      handleSubmit(event);
+    }
+  }
+
   return (
-    <div>SearchCollections</div>
+      <form onSubmit={handleSubmit}>
+        <input className='input' type="search" placeholder="Search NFT..." value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} onKeyDown={handleKeyPress} />
+      </form>
   )
 }
 
