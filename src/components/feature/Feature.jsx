a59d3919710024f9bf1857405c8c49e0b4886534
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2023-02-03 11:00:05 +0000

    smart contract

--- src/components/feature/Feature.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Feature = () => {
-  return (
-    <div>cta</div>
-  )
-}
-
-export default Feature
--- src/hooks/useUtilConnection.js
+++ src/hooks/useUtilConnection.js
@@ -4,6 +4,9 @@ import { networkConfigs } from '../helpers/networks';
 export const useUtilConnection = () => {
 
   const provider = ((window.ethereum != null) ? new ethers.providers.Web3Provider(window.ethereum) : ethers.providers.getDefaultProvider());
+  const contractABI = [ "JSONZ" ]; //TODO
+  const contractAddress = "0x..."; //TODO
+  const contractInstance = new ethers.Contract(contractAddress, contractABI, (isConnected(provider) ?  provider : null));
 
   const isConnected = () => {
     if(typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')){
@@ -66,5 +69,5 @@ export const useUtilConnection = () => {
     }
   }
 
-  return { provider, isConnected, connect, disconnect, getSigner, getChainId, getWalletAddress, switchNetwork };
+  return { provider, contractABI, contractAddress, contractInstance, isConnected, connect, disconnect, getSigner, getChainId, getWalletAddress, switchNetwork };
 };
