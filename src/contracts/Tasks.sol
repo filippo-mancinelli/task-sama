fbc6a77e2068473ec7f0fb796189f1dba7fcd807
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2023-02-20 12:01:51 +0000

    task

--- src/contracts/Tasks.sol
+++ src/contracts/Tasks.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.8.0;
 
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+
 contract Tasks {
+    using Counters for Counters.Counter;
+    Counters.Counter public totalTasks;
     address public owner;
 
     struct Task {
@@ -12,7 +16,6 @@ contract Tasks {
     }
 
     mapping (uint256 => Task) public tasks;
-    uint256 public totalTasks;
 
     constructor() {
         owner = msg.sender;
@@ -20,7 +23,7 @@ contract Tasks {
 
     function createTask(string memory _description, uint256 _rewardAmount) public {
         tasks[totalTasks] = Task(_description, _rewardAmount, msg.sender, new address[](0), address(0));
-        totalTasks++;
+        totalTasks.increment();
     }
 
     function submitTask(uint256 _taskId) public {
@@ -28,6 +31,7 @@ contract Tasks {
     }
 
     function chooseWinner(uint256 _taskId, address _winner) public {
+        require(msg.sender == tasks[_taskId].owner, "You cannot choose a winner of a task you don't own");
         tasks[_taskId].winner = _winner;
         payable(_winner).transfer(tasks[_taskId].rewardAmount);
     }
