cd3c89f29274a50ce54be361255019213ca122dc
filippo-mancinelli <filippo.mancinelli@studenti.unicam.it>
2023-05-15 00:07:28 +0200

    truffle migrate

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -18,15 +18,45 @@ module.exports = async function (deployer) {
   await tasks.createTask('TITLE 4', 'DESC 4', 'IPFS 4', { value: web3.utils.toWei("20", "ether") });
   await tasks.createTask('TITLE 5', 'DESC 5', 'IPFS 5', { value: web3.utils.toWei("25", "ether") });
 
-  //change accounts and participate to tasks
-  
+
+  // participate to tasks with various accounts
+  await tasks.participate(0)
+
+
+  /* 
+  //test send funds
+  const accounts = await web3.eth.getAccounts()
+  const sender = accounts[0]
+  const receiver = accounts[1]
+  const amount = web3.utils.toWei("200", "ether")
+  await web3.eth.sendTransaction({from: sender, to: receiver, value: amount})
+  */
 };
 
 
 
 /*
+const tasksama = await TaskSama.at("0x40B38908aF6312d3Ef6DAb3Ac3D16F01cfd27010");
+const tasks = await Tasks.at("0x5B346a3899BF4cDCa810E6CDf341b0b57A421A25");
+*/
 
-const tasksama = await TaskSama.at("0xdBC0e92de46BAC416cE40e04098C047922588886");
-const tasks = await Tasks.at("0x8a800458F062dbEAbA9ACFA6d4b604f7Ba1E12CC");
 
-*/ 
+/*
+### Get the list of available accounts:
+web3.eth.getAccounts().then(accounts => console.log(accounts))
+
+### Check the balance of an account:
+web3.eth.getBalance("0xYourAccountAddress").then(balance => console.log(web3.utils.fromWei(balance, 'ether') + " ETH"))
+
+### Send Ether from one account to another:
+web3.eth.sendTransaction({from: "0xSenderAccountAddress", to: "0xReceiverAccountAddress", value: web3.utils.toWei("1", "ether")})
+
+### call read-only SC function**
+const result = await contractInstance.myFunction.call({ from: accountToUse }) 
+
+### call state-changing SC function**
+const tx = await contractInstance.myFunction(param1, { from: accountToUse }) 
+
+
+
+*/
