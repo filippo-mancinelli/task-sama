b738a21fc34f717e6608e27bcf77e16752ec81f2
filippo-mancinelli <filippo.mancinelli@studenti.unicam.it>
2023-12-21 20:06:58 +0100

    change path

--- backend/index.js
+++ backend/index.js
@@ -40,9 +40,9 @@ app.listen(3000, () => {
 // Create HTTPS server only on production
 if (process.env.NODE_ENV === 'production') {
   const options = {
-    key: fs.readFileSync('/task-sama/backend/certificates/privkey.pem'),
-    cert: fs.readFileSync('/task-sama/backend/certificates/cert.pem'),
-    ca: fs.readFileSync('/task-sama/backend/certificates/chain.pem'),
+    key: fs.readFileSync('/certificates/privkey.pem'),
+    cert: fs.readFileSync('/certificates/cert.pem'),
+    ca: fs.readFileSync('/certificates/chain.pem'),
   };
 
   const httpsServer = https.createServer(options, app.callback());
